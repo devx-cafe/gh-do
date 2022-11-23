9717f517a802ca4b13de247e0c9c44e449f65357
Lars Kruse <lakruzz@inc-inc.dk>
2022-11-23 12:05:43 +0100

    spell mistake in email #6

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2022 Lars Kruse lakruzz'inc-inc.dk
+Copyright © 2022 Lars Kruse lakruzz@inc-inc.dk
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
